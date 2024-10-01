f8df5c04e309f2bfe6e0803755526ec41f258302
Jordan <99503935+jbiehl88@users.noreply.github.com>
2024-10-01 13:23:37 -0500

    cleared some duplicate jsx

--- App.js
+++ App.js
@@ -19,8 +19,6 @@ function App() {
 				<Location data={cities} location={location} setLocation={setLocation} />
 			</div>
 
-			<h1 className="title">REACTIVE WEATHER</h1>
-			<h3 className="subtitle">Up to the minute weather news</h3>
 			<div className="app">
 				{cities.map((card) => (
 					<WeatherCard data={card} />
